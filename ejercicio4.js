f2d232e6402716da21599064e0b91fd0070fb6fe
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-19 23:15:29 -0300

    Ejerc #4 v2 - uso función confirm()

--- ejercicio4.js
+++ ejercicio4.js
@@ -5,34 +5,30 @@ En caso no haya Condimentos pero Sí un Cubo de Caldo,
 entonces se prepara igual...
 Además informa el resultado...  
 */
-/* SE ASUME QUE EL ROBOT CUENTA CON AGUA DESDE UNA CONEXIÓN DIRECTA */
-/*
-let str = "Hola Mundo";
-let strMayus = str.toUpperCase();
-console.log(strMayus); // HOLA MUNDO
+/* SE ASUME QUE EL ROBOT CUENTA CON AGUA DESDE UNA CONEXIÓN DIRECTA
+   Y QUE SE LE DEBEN AGREGAR LOS DEMÁS INGREDIENTES
 */
-let respSiNo = "no";
-let hayVerduras = false;
-let hayCarne = false;
-let hayPollo = false;
-let hayCondimentos = false;
-let hayCuboDeCaldo = false;
 
-respSiNo = prompt("Quiere Hacer una Sopa? sino ingrese 'no'");
-if (respSiNo.toUpperCase() == "NO"){
+/*
+En la Versión 2 uso la función "confirm()"
+*/
+let quiereSopa      = false;
+let hayVerduras     = false;
+let hayCarne        = false;
+let hayPollo        = false;
+let hayCondimentos  = false;
+let hayCuboDeCaldo  = false;
+
+if (!(quiereSopa = confirm("Quiere Hacer una Sopa?"))) {
     document.write("OK! será en otra oportunidad...");
 } 
 else{
-    respSiNo = prompt ("Agrega Verduras? sino ingrese 'no'");
-    if (respSiNo.toUpperCase() == "NO"){hayVerduras = false} else {hayVerduras = true};
-    respSiNo = prompt ("Agrega Carne? sino ingrese 'no'");
-    if (respSiNo.toUpperCase() == "NO"){hayCarne = false} else {hayCarne = true};
-    respSiNo = prompt ("Agrega Pollo? sino ingrese 'no'");
-    if (respSiNo.toUpperCase() == "NO"){hayPollo = false} else {hayPollo = true};
-    respSiNo = prompt ("Agrega Condimentos? sino ingrese 'no'");
-    if (respSiNo.toUpperCase() == "NO"){hayCondimentos = false} else {hayCondimentos = true};
-    respSiNo = prompt ("Agrega Cubo de Caldo? sino ingrese 'no'");
-    if (respSiNo.toUpperCase() == "NO"){hayCuboDeCaldo = false} else {hayCuboDeCaldo = true};
+    hayVerduras     = confirm ("Agrega Verduras?");
+    hayCarne        = confirm ("Agrega Carne?");
+    hayPollo        = confirm ("Agrega Pollo?");
+    hayCondimentos  = confirm ("Agrega Condimentos?");
+    hayCuboDeCaldo  = confirm ("Agrega Cubo de Caldo?");
+    
     /* Verificamos si podemos hacer Sopa o no */
     if (hayVerduras&&(hayCarne||hayPollo)&&hayCondimentos){
         document.write("Hay SOPA con Carne o Pollo mas Condimentos");
